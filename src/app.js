d3efba3612a13e07824f5831aafe42caa96a4131
FranAcosta07 <acostafranmarce@gmail.com>
2024-10-09 19:42:17 -0300

    instala motor de plantillas

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ const app = express();
 const port = 3000;
 
 
+//configuración de los recursos estáticos
+app.use(express.static(path.join(__dirname,'..', 'public')));
 
+
+//configuración del motor de plantillas
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname, 'views'));
-
-
-
-
 
 
 
