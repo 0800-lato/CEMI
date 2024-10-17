dc7e7364312cc4d879e3dc799ad2bc8a843718ca
unknown <usuario@PUERTA18.CORP.AR>
2024-10-17 14:09:27 -0300

    fix (styles) corrige catalog

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -1,7 +1,8 @@
 const express = require ("express");
 const router =  express.Router();
 
-router.get("/", (req,res) => res.render("catalog"));
+router.get("/", (req,res) => res.render("home"));
+router.get("/catalog", (req,res) => res.render("catalog"));
 
 
 module.exports = router
