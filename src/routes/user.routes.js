d1bfb9280ef5a64d7d11bc65bb234243149145de
franacosta07 <acostafranmace@gmail.com>
2024-10-22 13:19:29 -0300

    corrige

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+/*const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController')
 
 // /users
-
+ QUEDA COMENTADO HASTA QUE CREEEMOS LOS CONTROLADORES
 router.get('/register',usersController.register);
 router.get('/login',usersController.login);
 router.get('/profile',usersController.profile)
 
 
-module.exports = router
+module.exports = router*/
