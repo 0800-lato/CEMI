35b6ee45bc45400aff72b2b1747285edb45cee04
Franacosta07 <acostafranmarce@gmail.com>
2024-10-22 16:18:23 -0300

    (feat) agrega rutas faltantes

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -4,8 +4,9 @@ const router =  express.Router();
 router.get("/", (req,res) => res.render("home"));
 router.get("/admin", (req,res) => res.render("admin"));
 router.get("/catalog", (req,res) => res.render("catalog"));
-router.get("/products-detail",(req,res)=> res.render("product-detail"));
-
-
+/**(RUTAS RECIEN AGREGADAS) */
+router.get("/products-detail",(req,res)=> res.render("product-detail")); 
+router.get("/login", (req,res) => res.render("login"));
+router.get("/register", (req,res) => res.render("register"));
 
 module.exports = router
