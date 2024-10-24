af59ca889e56b31aebda3d9cb5ac567ea5e8e971
UnknownN00N <federicogimenez99@hotmail.com>
2024-10-24 01:22:30 -0300

    admin-requests y admin-users especificadas

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -2,11 +2,12 @@ const express = require ("express");
 const router =  express.Router();
 
 router.get("/", (req,res) => res.render("home"));
-router.get("/admin", (req,res) => res.render("admin"));
+router.get("/admin/requests", (req,res) => res.render("admin-requests"));
 router.get("/catalog", (req,res) => res.render("catalog"));
 /**(RUTAS RECIEN AGREGADAS) */
 router.get("/products-detail",(req,res)=> res.render("product-detail")); 
 router.get("/login", (req,res) => res.render("login"));
 router.get("/register", (req,res) => res.render("register"));
+router.get("/admin/users", (req,res) => res.render("admin-users"));
 
 module.exports = router
